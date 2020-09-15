f403779858e6c24246481cafe4f221e8a4102570
root <root@ip-172-31-59-40.ec2.internal>
2020-09-15 01:06:23 +0000

    1er commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = var.region
+  profile = var.profile
+}
+
+resource "aws_instance" "prueba" {
+  ami = "ami-005bb0af4ade8c765"
+  instance_type = "t2.micro"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {}
+output "region" {
+ value = var.region
+}
+
+variable "profile" {}
+output "profile" {
+ value = var.profile
+}
