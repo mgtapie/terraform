594e4f207880aa84fa7ae35a7939f37063d08b48
mgtapie <mgtapie@gmail.com>
2020-09-20 19:00:54 -0300

    cambio de "Name" a "name"

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ provider "aws" {
 resource "aws_instance" "prueba" {
   ami = "ami-005bb0af4ade8c765"
   instance_type = "t2.micro"
-  Name = "instancia_profile_cambiado"
+  name = "instancia_profile_cambiado"
 }
